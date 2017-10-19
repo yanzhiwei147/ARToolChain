249e7f210ca9e0eaf63fc202d44dc32f34f36e80
arida <yanzhiwei147@gmail.com>
2017-10-19 19:02:58 +0800

    Remove not required code.

--- JSONTools/Application/SourceEditorCommand.m
+++ JSONTools/Application/SourceEditorCommand.m
@@ -17,30 +17,4 @@
              @"format.json.prettity": jsonHandler
     };
 }
-
-//- (void)performCommandWithInvocation:(XCSourceEditorCommandInvocation *)invocation completionHandler:(void (^)(NSError * _Nullable nilOrError))completionHandler {
-//
-//    XCSourceTextBuffer *buffer = invocation.buffer;
-//
-//    // get all editor content
-//    NSString *editorContent = buffer.completeBuffer;
-//
-//    NSError *error = nil;
-//    id jsonValue = [editorContent ar_jsonValueWithError:&error];
-//    if (!jsonValue) {
-//        completionHandler(error);
-//        return;
-//    }
-//
-//    NSString *formatedString = [jsonValue ar_jsonRepresentationWithOptions:NSJSONWritingPrettyPrinted error:&error];
-//    if (!formatedString) {
-//        completionHandler(error);
-//        return;
-//    }
-//
-//    buffer.completeBuffer = formatedString;
-//
-//    completionHandler(nil);
-//}
-
 @end
